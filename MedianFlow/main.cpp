c705f8b8c67f259044c3a1086b0e10d0e7fcd90b
Orthocenter <chenyuxin.mail@gmail.com>
2014-10-07 15:36:44 +0800

    Initial Commit

---
+++ MedianFlow/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MedianFlow
+//
+//  Created by 陈裕昕 on 10/7/14.
+//  Copyright (c) 2014 陈裕昕. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
