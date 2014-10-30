ad549d7af10f305d836f5ba6fd1f5810461a9f6d
Orthocenter <chenyuxin.mail@gmail.com>
2014-10-30 20:06:10 +0800

    fix some issue caused by data type

---
+++ MedianFlow/MedianFlow.cpp
@@ -0,0 +1,9 @@
+//
+//  MedianFlow.cpp
+//  MedianFlow
+//
+//  Created by 陈裕昕 on 10/29/14.
+//  Copyright (c) 2014 陈裕昕. All rights reserved.
+//
+
+#include "MedianFlow.h"
---
+++ MedianFlow/MedianFlow.h
@@ -0,0 +1,14 @@
+//
+//  MedianFlow.h
+//  MedianFlow
+//
+//  Created by 陈裕昕 on 10/29/14.
+//  Copyright (c) 2014 陈裕昕. All rights reserved.
+//
+
+#ifndef __MedianFlow__MedianFlow__
+#define __MedianFlow__MedianFlow__
+
+#include <stdio.h>
+
+#endif /* defined(__MedianFlow__MedianFlow__) */
